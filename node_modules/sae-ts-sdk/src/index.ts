4daccf287818aa58ac2473d95f015b4b71c0be65
alejandro <alejando.miranda.org@outlook.com>
2026-02-28 18:24:31 -0600

    feat(typescript-sdk): Migrate to ESM (v0.2.0)
    - Changed tsconfig module from commonjs to ES2020 (native ESM output)
    - Added moduleResolution: bundler for Vite/Astro compat
    - Added declarationMap and sourceMap for better debugging
    - Bumped version to 0.2.0
    - Added type: module to package.json
    - Removed require export (ESM-only)
    - Updated .gitignore to exclude dist/ and node_modules at all levels

--- node_modules/sae-ts-sdk/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './client/SaeClient';
-export * from './models/requests';
-export * from './models/responses';
-export * from './models/enums';
-export * from './models/logs';
-export * from './services/systemLogService';
-export * from './utils/DocumentCalculator';
--- typescript/dist/services/systemLogService.d.ts
+++
@@ -1,9 +0,0 @@
-import { ApiClient } from '../client/SaeClient';
-import { LogFilter, LogResponse } from '../models/logs';
-export declare class SystemLogService {
-    private client;
-    constructor(client: ApiClient);
-    getLogs(filter: LogFilter): Promise<LogResponse>;
-    getLogDetails(id: string): Promise<any>;
-    getDeveloperLogs(filter: LogFilter): Promise<LogResponse>;
-}
